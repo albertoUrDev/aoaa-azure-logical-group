0cb4a050061b7d08beda688039e03decb49aaf13
David Liu <liudav@microsoft.com>
2021-10-15 10:54:39 -0700

    pushed

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ function App() {
   const [hideFirstDialog, { toggle: toggleHideFirstDialog }] = useBoolean(true);
   const [hideSecondDialog, { toggle: toggleHideSecondDialog }] =
     useBoolean(true);
-  const [hideThirdDialog, { toggle: toggleHideThirdDialog }] = useBoolean(true);
+    const [hideThirdDialog, { toggle: toggleHideThirdDialog }] = useBoolean(true);
+
   const options = [
     { key: "OptimumGroup", text: "Optimum Group" },
     { key: "SomeGroup", text: "SomeGroup" },
@@ -35,9 +36,45 @@ function App() {
   };
   const dialog3ContentProps = {
     title: "Alerts",
-  };
+    };
+
+    const KustoClient = require("azure-kusto-data").Client;
+    const KustoConnectionStringBuilder = require("azure-kusto-data").KustoConnectionStringBuilder;
+    const ClientRequestProperties = require("azure-kusto-data").ClientRequestProperties;
+    const { v4: uuidv4 } = require('uuid');
+
+    const clusterConectionString = "https://cognitoprod.westus2.kusto.windows.net";
+    const database = "Common";
+    const table = "Common_Cloudfit_Reco_Daily_Comparison";
+
+    const kcs = KustoConnectionStringBuilder.withAadDeviceAuthentication(clusterConectionString);
+    const kustoClient = new KustoClient(kcs);
+
+    async function startfunction() {
+        // function body
+        // kusto connection
+        const ClientRequestProperties = require("azure-kusto-data").ClientRequestProperties;
+        const Client = require("azure-kusto-data").Client;
+
+        const client = new Client("https://cognitoprod.westus2.kusto.windows.net", "d5b95c14-4d44-42d7-a8e7-78be85ddeb1f", "PPvZz8591RuVHre5X9q~g43H~c-~~CTRbF", "72f988bf-86f1-41af-91ab-2d7cd011db47");
+        const query = `
+Common_Cloudfit_Reco_Daily_Comparison | take 1
+`;
+        const clientRequestProps = new ClientRequestProperties();
+        clientRequestProps.setOption("servertimeout", 1000 * 60);
+        clientRequestProps.setParameter("amount", 100);
+        const results = await client.executeQuery("Common", query, clientRequestProps);
+    }
+
   return (
     <>
+        <div>
+            <DefaultButton
+                secondaryText="Opens the Sample Dialog2"
+                  onClick={startfunction}
+                text="Budget Groups2.5"
+            />
+        </div>
       <div>
         <DefaultButton
           secondaryText="Opens the Sample Dialog"
